5114684b0b17106583da7b11797666548d855a6c
Pontius <zponce81@firemail.cc>
2020-08-11 13:17:26 -0400

    Fixed README & added notes

--- game.py
+++ game.py
@@ -4,28 +4,34 @@ import sys
 import time
 import random
 
+##Establish the hangman class
 class Hangman:
- 
+    
+    ##Create an array full of potential words
     words = [ "hat", "constantinople", "hispana", "rome", "arch", "athens",
             "paris", "lima", "guayaquil", "portoviejo", "vienna", "seville",
             "valladolid", "oveido", "porto", "lisbon", "pavia", "aquileia"]
     
-    # Not sure if an alphabet is necessary
+    ##Not sure if an alphabet is necessary
     alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
             "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
             "x", "y", "z"]
     
     Num_guesses = 7
-
+    
+    ##Initialize object with the following parameters: self (ie. Hangman, & name)
     def __init__(self, nm):
-        
+        ##Store the given name into a variable
         self.name = nm
-        
-        word = random.choice(Hangman.words) 
-        secretword = word.splitlines()
-        
-        #print(secretword)
-
+        ##Fetches a random word from the static array 'words' 
+        word = random.choice(self.words)
+        ##Split said word into seperate strings
+        secretword = self.split(word)
+        print(secretword)
+        ##Start the pregame
+        self.Greeting()
+    
+    ##Pre-game state in which the user is given the option to begin or exit.
     def Greeting(self):
     
         print ("Hello, " + self.name + "!", "Time to play hangman!")
@@ -33,24 +39,27 @@ class Hangman:
         via = input(">>> ")
         
         if via == 'start':
-            Hangman.Gamestate()
+            self.Gamestate()
         elif via == 'exit':
             quit()
         else:
             print('I don\'t understand what that means')
-
+            return 
+    
+    ##Game state in which the player will provide inputs that will match the strings or not
     def Gamestate(self):
          
         time.sleep(1)
         print('Take a guess.....')
         time.sleep(0.5)
 
-        while Num_guesses < 7:
+        while self.Num_guesses < 7:
 
             pass
-
+    
+    def split(self, word):
+        return [char for char in word]
 
 name = input("Please enter your name >>> ")
 
-start = Hangman(name)    
-start.Greeting()    
+start = Hangman(name)        
