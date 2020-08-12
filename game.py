aa1cdd9ba6b191f49791d10f8ac812b33088bdfb
Pontius <zponce81@firemail.cc>
2020-08-12 12:56:51 -0400

    Updated the Gamestate in Class Hangman

--- game.py
+++ game.py
@@ -3,22 +3,20 @@ import math
 import sys
 import time
 import random
-
+import functools
 ##Establish the hangman class
 class Hangman:
     
+    ##Add a score
+    score = 0 
     ##Create an array full of potential words
     words = [ "hat", "constantinople", "hispana", "rome", "arch", "athens",
             "paris", "lima", "guayaquil", "portoviejo", "vienna", "seville",
             "valladolid", "oveido", "porto", "lisbon", "pavia", "aquileia"]
-    
-    ##Not sure if an alphabet is necessary
-    alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
-            "x", "y", "z"]
-    
-    Num_guesses = 7
-    
+    ##Wrong letters go below
+    wrong_letters = [] 
+    ##Correct letters go below
+    correct_letters = []
     ##Initialize object with the following parameters: self (ie. Hangman, & name)
     def __init__(self, nm):
         ##Store the given name into a variable
@@ -26,40 +24,91 @@ class Hangman:
         ##Fetches a random word from the static array 'words' 
         word = random.choice(self.words)
         ##Split said word into seperate strings
-        secretword = self.split(word)
-        print(secretword)
+        self.secretword = self.split(word)
+        print(self.secretword)
         ##Start the pregame
-        self.Greeting()
+        self.Pregame()
     
     ##Pre-game state in which the user is given the option to begin or exit.
-    def Greeting(self):
+    def Pregame(self):
     
         print ("Hello, " + self.name + "!", "Time to play hangman!")
         print ("Enter 'start' to begin. Otherwise, enter 'exit'")
-        via = input(">>> ")
-        
-        if via == 'start':
-            self.Gamestate()
-        elif via == 'exit':
-            quit()
-        else:
-            print('I don\'t understand what that means')
-            return 
+       
+    ##A while loop to wait until the user decides to start the game or not.
+        while True:
+            via = input(">>> ")    
+            
+            if via == 'start':
+                self.Gamestate()
+            elif via == 'exit':
+                quit()
+            else:
+                print('I don\'t understand what that means')
     
     ##Game state in which the player will provide inputs that will match the strings or not
     def Gamestate(self):
-         
-        time.sleep(1)
-        print('Take a guess.....')
-        time.sleep(0.5)
+      
+       #Figure out how to display the used letters array! 
+       # print(self.used_letters)
+       
+        while True:
+            
+            time.sleep(1)
+            userletter = input('Enter a Letter: ')
+            time.sleep(0.5)
+        
+            if len(self.correct_letters) != len(self.secretword):
+                if len(self.wrong_letters) !=  7:
+                    if userletter in self.secretword:
+                        print("Congrats, you win")
+                        self.correct_letters.append(userletter)
+                        print(self.correct_letters)
+                    elif userletter not in  self.secretword:
+                        print("Too bad m8")
+                        self.wrong_letters.append(userletter)
+                        print(self.wrong_letters)
+                    elif userletter in self.used_letters or self.correct_letters:
+                        print("You already inputed that letter, dofus!")
+                else:
+                    self.Game_over(1)
+            else:
+                self.Game_over(0)
 
-        while self.Num_guesses < 7:
+    def Game_over(self, x):
 
-            pass
-    
+        while True:
+            
+            if x ==  1:
+                print('You lose!')
+            else:
+                print('You win!')
+            
+            option = input('Play again? >>> ')
+
+            if option == 'yes':
+                self.Pregame()
+            elif option == 'no':
+                quit()
+            else:
+                print('I don\'t know what that means!')
+
+
     def split(self, word):
         return [char for char in word]
 
+    def sub(x, y):
+        x = x - y 
+        return x 
+
+
+
+## Initialize pygame
+#pygame.init()
+
+
+##Initialize Game itself
+#Figure out how to prompt the user inside the pygame window!
 name = input("Please enter your name >>> ")
 
 start = Hangman(name)        
